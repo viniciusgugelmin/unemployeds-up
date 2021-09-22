2dc6df305a32e256d226bf596189d68009d8b404
unknown <vinigugelmin@gmail.com>
2021-09-22 20:46:15 -0300

    Login page front

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -3,12 +3,10 @@ import { Component, OnInit } from "@angular/core";
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
-    styleUrls: [],
 })
 export class AppComponent implements OnInit {
     title = "Unemployeds";
 
-    ngOnInit(): void {
-        console.log("Hello world!");
-    }
+    // mounted
+    ngOnInit(): void {}
 }
