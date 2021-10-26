ac4a1989340a9d55c30ed38394d053bb847358cf
unknown <vinigugelmin@gmail.com>
2021-10-25 21:20:10 -0300

    Delete administrator seeders

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -9,35 +9,5 @@ export class AppComponent implements OnInit {
     title = "Unemployeds";
 
     // mounted
-    ngOnInit(): void {
-        this.checkIfExistsAdministrator();
-    }
-
-    checkIfExistsAdministrator() {
-        axios
-            .get(`https://localhost:5001/api/administrator/`)
-            .then((response) => {
-                if (response.data.length === 0) {
-                    this.seedAdministrator();
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-
-    seedAdministrator() {
-        axios
-            .post(`https://localhost:5001/api/administrator/`, {
-                Name: "Lorem Ipsum",
-                Email: "test@gmail.com",
-                Password: "123456",
-            })
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
+    ngOnInit(): void {}
 }
